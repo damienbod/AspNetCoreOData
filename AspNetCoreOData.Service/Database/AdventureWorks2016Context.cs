061f90a338599bdba201193c6a7d8c299ac49685
damienbod <damien_bod@hotmail.com>
2020-11-22 21:29:44 +0100

    code clean up

--- AspNetCoreOData.Service/Database/AdventureWorks2016Context.cs
+++ AspNetCoreOData.Service/Database/AdventureWorks2016Context.cs
@@ -114,7 +114,6 @@ namespace AspNetCoreOData.Service.Database
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=AdventureWorks2016;Integrated Security=True");
             }
         }
