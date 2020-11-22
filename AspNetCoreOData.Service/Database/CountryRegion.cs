970ca302baf95613181fb104ebb1fdbee6eab282
damienbod <damien_bod@hotmail.com>
2020-11-22 18:10:49 +0100

    adding keys

--- AspNetCoreOData.Service/Database/CountryRegion.cs
+++ AspNetCoreOData.Service/Database/CountryRegion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -14,6 +15,7 @@ namespace AspNetCoreOData.Service.Database
             StateProvinces = new HashSet<StateProvince>();
         }
 
+        [Key]
         public string CountryRegionCode { get; set; }
         public string Name { get; set; }
         public DateTime ModifiedDate { get; set; }
